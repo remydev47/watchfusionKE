121360ccc8af313951b3509bfae4c6e07326da4c
remydev47 <brianrimidev47@gmail.com>
2026-03-26 13:23:17 +0300

    chore: Remove Wix API proxy rewrites from Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -21,26 +21,6 @@ const nextConfig = {
     ],
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/_api/:path*",
-        destination: "https://469ac00d-9631-4a9d-b69b-9b0c047b99cf.wixsite.com/_api/:path*",
-      },
-      {
-        source: "/__ecom/:path*",
-        destination: "https://469ac00d-9631-4a9d-b69b-9b0c047b99cf.wixsite.com/__ecom/:path*",
-      },
-      {
-        source: "/_partials/:path*",
-        destination: "https://469ac00d-9631-4a9d-b69b-9b0c047b99cf.wixsite.com/_partials/:path*",
-      },
-      {
-        source: "/_serverless/:path*",
-        destination: "https://469ac00d-9631-4a9d-b69b-9b0c047b99cf.wixsite.com/_serverless/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
